281c013d6ff75f181e2423c0987bf8d53ad5ec0a
nghia <naovu@protonmail.com>
2022-02-07 15:10:39 +0700

    Codebase: Add React-Router and favicon

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+const LIST_ROUTES = [
+  {
+    path: "*",
+    component: React.lazy(() => import("../containers/Homepage")),
+  },
+];
+
+export const listRoute = () => {
+  return LIST_ROUTES.map((route, index) => {
+    return (
+      <Route
+        key={index}
+        path={route.path}
+        element={
+          <React.Suspense fallback={<div style={{ height: "100vh" }}></div>}>
+            <route.component />
+          </React.Suspense>
+        }
+      />
+    );
+  });
+};
