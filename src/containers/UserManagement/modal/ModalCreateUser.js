1c87d2f7c856cf86c5ee2678015574e668d8ae23
nghia <naovu@protonmail.com>
2022-02-28 21:10:52 +0700

    Reformat code

--- src/containers/UserManagement/modal/ModalCreateUser.js
+++ src/containers/UserManagement/modal/ModalCreateUser.js
@@ -1,37 +1,39 @@
 import {
-    FormControl,
-    FormControlLabel,
-    FormHelperText,
-    FormLabel,
-    InputLabel,
-    Modal,
-    RadioGroup,
-    Typography
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  InputLabel,
+  Modal,
+  RadioGroup,
+  Typography,
 } from "@mui/material";
 import Box from "@mui/material/Box";
-import {Input, Radio} from "@mui/icons-material";
+import { Input, Radio } from "@mui/icons-material";
 import LoginForm from "../../../components/LoginForm";
 const style = {
-    position: 'absolute',
-    top: '40%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 'auto',
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
 };
 const ModalCreateUser = (props) => {
-    const {visible, onClose} = props
-    return <Modal
-        open={visible}
-        onClose={()=>onClose()}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+  const { visible, onClose } = props;
+  return (
+    <Modal
+      open={visible}
+      onClose={() => onClose()}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
     >
-        <Box sx={style}>
-            <LoginForm />
-        </Box>
+      <Box sx={style}>
+        <LoginForm />
+      </Box>
     </Modal>
-}
+  );
+};
 export default ModalCreateUser;
--- src/containers/UserManagement/model/Column.js
+++ src/containers/UserManagement/model/Column.js
@@ -1,49 +1,47 @@
 import Button from "@mui/material/Button";
-import {IconButton} from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import InfoIcon from '@mui/icons-material/Info';
-
-
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import InfoIcon from "@mui/icons-material/Info";
 
 export const Column = [
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        disableColumnMenu: true,
-        sortable: false,
-        flex: 1,
-        width:'auto',
-    },
-    {
-        field: 'dateOfBirth',
-        headerName: 'Birthday',
-        disableColumnMenu: true,
-        sortable: false,
-        type: 'number',
-        width: 180
-    },
-    {
-        field: 'Email',
-        headerName: 'email',
-        disableColumnMenu: true,
-        sortable: false,
-        type: 'number',
-        width: 180
-    },
-    {
-        field: 'Department',
-        headerName: 'department',
-        disableColumnMenu: true,
-        sortable: false,
-        type: 'number',
-        width: 180
-    },
-    {
-        field: 'Role',
-        headerName: 'role',
-        disableColumnMenu: true,
-        sortable: false,
-        width: 180
-    }
-]
+  {
+    field: "fullName",
+    headerName: "Full name",
+    disableColumnMenu: true,
+    sortable: false,
+    flex: 1,
+    width: "auto",
+  },
+  {
+    field: "dateOfBirth",
+    headerName: "Birthday",
+    disableColumnMenu: true,
+    sortable: false,
+    type: "number",
+    width: 180,
+  },
+  {
+    field: "Email",
+    headerName: "email",
+    disableColumnMenu: true,
+    sortable: false,
+    type: "number",
+    width: 180,
+  },
+  {
+    field: "Department",
+    headerName: "department",
+    disableColumnMenu: true,
+    sortable: false,
+    type: "number",
+    width: 180,
+  },
+  {
+    field: "Role",
+    headerName: "role",
+    disableColumnMenu: true,
+    sortable: false,
+    width: 180,
+  },
+];
