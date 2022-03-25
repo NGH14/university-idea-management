b6d5a05b4423534271ec3f678c11674b7ef89933
Henry-idiotium <quocdat.381872@gmail.com>
2022-03-25 18:12:27 +0700

    chore: :necktie: Remove logic of Update Password after from email

--- src/containers/UpdatePassword/ValidateAuthInfo/index.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-import { URL_PATHS } from "../../../common/env";
-import Login from "../../Login";
-
-export default function ValidateAuthInfo() {
-  let { token } = useParams();
-  return <Login returnUrl={`${URL_PATHS.UPDATE_PWD}/${token}`}></Login>;
-}
