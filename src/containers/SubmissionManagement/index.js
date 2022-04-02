01e6cb24d51dad93af38e3be095ee5d09ef3d925
nghia <naovu@protonmail.com>
2022-04-03 05:55:15 +0700

    MODIFY: Loading spinner

--- src/containers/SubmissionManagement/index.js
+++ src/containers/SubmissionManagement/index.js
@@ -11,11 +11,11 @@ import { IconButton } from "@mui/material";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import {
-	GridToolbarColumnsButton,
-	GridToolbarContainer,
-	GridToolbarDensitySelector,
-	GridToolbarExport,
-	GridToolbarFilterButton,
+  GridToolbarColumnsButton,
+  GridToolbarContainer,
+  GridToolbarDensitySelector,
+  GridToolbarExport,
+  GridToolbarFilterButton,
 } from "@mui/x-data-grid";
 import { DataGridPro, GridActionsCellItem } from "@mui/x-data-grid-pro";
 import * as React from "react";
@@ -32,269 +32,269 @@ import { toast } from "react-toastify";
 import { dataDemo_submissions } from "./FakeData/Submissions";
 
 const toastMessages = {
-	WAIT: "Please wait...",
-	SUC_SUB_ADDED: "Create submission successful !!",
-	SUC_SUB_EDITED: "Update submission successful !!",
-	SUC_SUB_DEL: "Delete submission successful !!",
-	ERR_SERVER_ERROR: "Something went wrong, please try again !!",
+  WAIT: "Please wait...",
+  SUC_SUB_ADDED: "Create submission successful !!",
+  SUC_SUB_EDITED: "Update submission successful !!",
+  SUC_SUB_DEL: "Delete submission successful !!",
+  ERR_SERVER_ERROR: "Something went wrong, please try again !!",
 };
 
 function SubmissionManagement() {
-	const { state } = useContext(UserContext);
-	const [data, setData] = useState([]);
-	const [rowId, setRowId] = useState(null);
-	const navigate = useNavigate();
+  const { state } = useContext(UserContext);
+  const [data, setData] = useState([]);
+  const [rowId, setRowId] = useState(null);
+  const navigate = useNavigate();
 
-	const [status, setStatus] = useState({
-		visibleModal: false,
-		action: "create",
-	});
+  const [status, setStatus] = useState({
+    visibleModal: false,
+    action: "create",
+  });
 
-	const [pagination, setPagination] = useState({
-		pageSize: 10,
-		page: 0,
-	});
+  const [pagination, setPagination] = useState({
+    pageSize: 10,
+    page: 0,
+  });
 
-	const [actionUser, setActionUser] = useState(null);
-	const [tableToolBar, setTableToolBar] = useState(false);
+  const [actionUser, setActionUser] = useState(null);
+  const [tableToolBar, setTableToolBar] = useState(false);
 
-	useEffect(() => {
-		if (DEV_CONFIGS.IS_DEV) {
-			setData(dataDemo_submissions);
-			setRowId(null);
-			return;
-		}
+  useEffect(() => {
+    if (DEV_CONFIGS.IS_DEV) {
+      setData(dataDemo_submissions);
+      setRowId(null);
+      return;
+    }
 
-		loadData();
-	}, [pagination]);
+    loadData();
+  }, [pagination]);
 
-	const openUserAction = Boolean(actionUser);
-	const handleOnClickToolBar = () => setTableToolBar((pre) => !pre);
+  const openUserAction = Boolean(actionUser);
+  const handleOnClickToolBar = () => setTableToolBar((pre) => !pre);
 
-	const handleClick = (event) => setActionUser(event.currentTarget);
-	const handleClose = () => setActionUser(null);
+  const handleClick = (event) => setActionUser(event.currentTarget);
+  const handleClose = () => setActionUser(null);
 
-	const columns = [
-		...Column,
-		{
-			field: "actions",
-			headerName: "Action",
-			width: 75,
-			type: "actions",
-			disableColumnMenu: true,
-			sortable: false,
-			getActions: (params) => [
-				<GridActionsCellItem
-					icon={<InfoOutlinedIcon color={"info"} />}
-					label="Detail"
-					onClick={() => navigate(`${URL_PATHS.MANAGE_SUB}/${params.id}`)}
-					showInMenu
-				/>,
+  const columns = [
+    ...Column,
+    {
+      field: "actions",
+      headerName: "Action",
+      width: 75,
+      type: "actions",
+      disableColumnMenu: true,
+      sortable: false,
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<InfoOutlinedIcon color={"info"} />}
+          label="Detail"
+          onClick={() => navigate(`${URL_PATHS.MANAGE_SUB}/${params.id}`)}
+          showInMenu
+        />,
 
-				<GridActionsCellItem
-					icon={<EditIcon color={"secondary"} />}
-					label="Update"
-					onClick={() => onOpenModal(params.id, "update")}
-					showInMenu
-				/>,
+        <GridActionsCellItem
+          icon={<EditIcon color={"secondary"} />}
+          label="Update"
+          onClick={() => onOpenModal(params.id, "update")}
+          showInMenu
+        />,
 
-				<GridActionsCellItem
-					icon={<DeleteIcon />}
-					disabled={state?.dataUser?.id === params.id ? true : false}
-					label="Delete"
-					onClick={() => onDelete(params.id)}
-					showInMenu
-				/>,
-			],
-		},
-	];
-	const loadData = async () => {
-		await AuthRequest.get(API_PATHS.ADMIN.MANAGE_SUB, {
-			params: {
-				page: pagination.page + 1,
-				page_size: pagination.pageSize,
-			},
-		})
-			.then((res) => {
-				setData(res?.data?.result?.rows);
-				setRowId(null);
-			})
-			.catch(() =>
-				toast.error(toastMessages.ERR_SERVER_ERROR, {
-					style: { width: "auto" },
-				}),
-			);
-	};
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          disabled={state?.dataUser?.id === params.id ? true : false}
+          label="Delete"
+          onClick={() => onDelete(params.id)}
+          showInMenu
+        />,
+      ],
+    },
+  ];
+  const loadData = async () => {
+    await AuthRequest.get(API_PATHS.ADMIN.MANAGE_SUB, {
+      params: {
+        page: pagination.page + 1,
+        page_size: pagination.pageSize,
+      },
+    })
+      .then((res) => {
+        setData(res?.data?.result?.rows);
+        setRowId(null);
+      })
+      .catch(() =>
+        toast.error(toastMessages.ERR_SERVER_ERROR, {
+          style: { width: "auto" },
+        })
+      );
+  };
 
-	const onOpenModal = (id, action) => {
-		if (id) {
-			setRowId(id);
-		}
-		setStatus({ ...status, visibleModal: true, action });
-	};
+  const onOpenModal = (id, action) => {
+    if (id) {
+      setRowId(id);
+    }
+    setStatus({ ...status, visibleModal: true, action });
+  };
 
-	const onDelete = async (id) => {
-		toast
-			.promise(
-				AuthRequest.delete(`${API_PATHS.ADMIN.MANAGE_SUB}/${id}`)
-					.then(() => sleep(700))
-					.catch(),
-				{
-					pending: toastMessages.WAIT,
-					success: toastMessages.SUC_SUB_DEL,
-					error: toastMessages.ERR_SERVER_ERROR,
-				},
-			)
-			.then(() => {
-				setStatus({ ...status, visibleModal: false });
-				loadData();
-			});
-	};
+  const onDelete = async (id) => {
+    toast
+      .promise(
+        AuthRequest.delete(`${API_PATHS.ADMIN.MANAGE_SUB}/${id}`)
+          .then(() => sleep(700))
+          .catch(),
+        {
+          pending: toastMessages.WAIT,
+          success: toastMessages.SUC_SUB_DEL,
+          error: toastMessages.ERR_SERVER_ERROR,
+        }
+      )
+      .then(() => {
+        setStatus({ ...status, visibleModal: false });
+        loadData();
+      });
+  };
 
-	const onUpdate = async (value) => {
-		toast
-			.promise(
-				AuthRequest.put(`${API_PATHS.ADMIN.MANAGE_SUB}/${value?.id}`, value)
-					.then(() => sleep(700))
-					.catch(),
-				{
-					pending: toastMessages.WAIT,
-					success: toastMessages.SUC_SUB_EDITED,
-					error: toastMessages.ERR_SERVER_ERROR,
-				},
-			)
-			.then(() => {
-				setStatus({ ...status, visibleModal: false });
-				loadData();
-			});
-	};
+  const onUpdate = async (value) => {
+    toast
+      .promise(
+        AuthRequest.put(`${API_PATHS.ADMIN.MANAGE_SUB}/${value?.id}`, value)
+          .then(() => sleep(700))
+          .catch(),
+        {
+          pending: toastMessages.WAIT,
+          success: toastMessages.SUC_SUB_EDITED,
+          error: toastMessages.ERR_SERVER_ERROR,
+        }
+      )
+      .then(() => {
+        setStatus({ ...status, visibleModal: false });
+        loadData();
+      });
+  };
 
-	const onCreate = async (value) => {
-		toast
-			.promise(
-				AuthRequest.post(API_PATHS.ADMIN.MANAGE_SUB, value)
-					.then(() => sleep(700))
-					.catch(),
-				{
-					pending: toastMessages.WAIT,
-					success: toastMessages.SUC_SUB_ADDED,
-					error: toastMessages.ERR_SERVER_ERROR,
-				},
-			)
-			.then(() => {
-				setStatus({ ...status, visibleModal: false });
-				loadData();
-			});
-	};
+  const onCreate = async (value) => {
+    toast
+      .promise(
+        AuthRequest.post(API_PATHS.ADMIN.MANAGE_SUB, value)
+          .then(() => sleep(700))
+          .catch(),
+        {
+          pending: toastMessages.WAIT,
+          success: toastMessages.SUC_SUB_ADDED,
+          error: toastMessages.ERR_SERVER_ERROR,
+        }
+      )
+      .then(() => {
+        setStatus({ ...status, visibleModal: false });
+        loadData();
+      });
+  };
 
-	const onCloseModal = () => {
-		if (rowId) {
-			setRowId(null);
-		}
-		setStatus({
-			...status,
-			visibleModal: false,
-		});
-	};
+  const onCloseModal = () => {
+    if (rowId) {
+      setRowId(null);
+    }
+    setStatus({
+      ...status,
+      visibleModal: false,
+    });
+  };
 
-	const CustomToolbar = () => {
-		return (
-			<GridToolbarContainer
-				sx={{ fontWeight: 700, display: "flex", justifyContent: "ceter" }}
-			>
-				<GridToolbarColumnsButton />
-				<GridToolbarFilterButton />
-				<GridToolbarDensitySelector />
-				<GridToolbarExport printOptions={{ disableToolbarButton: true }} />
-			</GridToolbarContainer>
-		);
-	};
+  const CustomToolbar = () => {
+    return (
+      <GridToolbarContainer
+        sx={{ fontWeight: 700, display: "flex", justifyContent: "ceter" }}
+      >
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+        <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
+      </GridToolbarContainer>
+    );
+  };
 
-	const renderModal = () => {
-		return (
-			<ModalSubmissionManagement
-				visible={status.visibleModal}
-				action={status.action}
-				onClose={onCloseModal}
-				rowId={rowId}
-				onCreate={onCreate}
-				onUpdate={onUpdate}
-			/>
-		);
-	};
+  const renderModal = () => {
+    return (
+      <ModalSubmissionManagement
+        visible={status.visibleModal}
+        action={status.action}
+        onClose={onCloseModal}
+        rowId={rowId}
+        onCreate={onCreate}
+        onUpdate={onUpdate}
+      />
+    );
+  };
 
-	const onChangePagination = (pageSize, page) => {
-		setPagination({ page, pageSize });
-	};
+  const onChangePagination = (pageSize, page) => {
+    setPagination({ page, pageSize });
+  };
 
-	const renderTop = () => {
-		return (
-			<div className="managementsubmission_title">
-				<div className="managementsubmission_heading">
-					<h2>Management Submission</h2>
-					<Tooltip title="Table Tool Bar">
-						<IconButton onClick={handleOnClickToolBar}>
-							<MoreVertIcon />
-						</IconButton>
-					</Tooltip>
-				</div>
+  const renderTop = () => {
+    return (
+      <div className="managementsubmission_title">
+        <div className="managementsubmission_heading">
+          <h2>Management Submission</h2>
+          <Tooltip title="Table Tool Bar">
+            <IconButton onClick={handleOnClickToolBar}>
+              <MoreVertIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
 
-				<Button
-					variant="contained"
-					endIcon={<AddCircleOutlineIcon />}
-					onClick={() => onOpenModal(null, "create")}
-				>
-					Create
-				</Button>
-			</div>
-		);
-	};
+        <Button
+          variant="contained"
+          endIcon={<AddCircleOutlineIcon />}
+          onClick={() => onOpenModal(null, "create")}
+        >
+          Create
+        </Button>
+      </div>
+    );
+  };
 
-	const renderContent = () => {
-		return (
-			<div className="managementsubmission_table">
-				<DataGridPro
-					components={{
-						NoRowsOverlay: CustomNoRowsOverlay,
-						ColumnSortedDescendingIcon: () => (
-							<ExpandMoreIcon className="icon" />
-						),
-						ColumnSortedAscendingIcon: () => (
-							<ExpandLessIcon className="icon" />
-						),
-						Toolbar: tableToolBar && CustomToolbar,
-					}}
-					rows={data}
-					columns={columns}
-					pagination={true}
-					cell--textCenter
-					pageSize={pagination.pageSize}
-					page={pagination.page}
-					initialState={{ pinnedColumns: { right: ["actions"] } }}
-					onPageSizeChange={(pageSize) =>
-						onChangePagination(pageSize, pagination.page)
-					}
-					onPageChange={(page) => onChangePagination(pagination.pageSize, page)}
-					style={{ minHeight: "600px" }}
-					rowsPerPageOptions={[10, 25, 50, 100]}
-				/>
-			</div>
-		);
-	};
+  const renderContent = () => {
+    return (
+      <div className="managementsubmission_table">
+        <DataGridPro
+          components={{
+            NoRowsOverlay: CustomNoRowsOverlay,
+            ColumnSortedDescendingIcon: () => (
+              <ExpandMoreIcon className="icon" />
+            ),
+            ColumnSortedAscendingIcon: () => (
+              <ExpandLessIcon className="icon" />
+            ),
+            Toolbar: tableToolBar && CustomToolbar,
+          }}
+          rows={data}
+          columns={columns}
+          pagination={true}
+          cell--textCenter
+          pageSize={pagination.pageSize}
+          page={pagination.page}
+          initialState={{ pinnedColumns: { right: ["actions"] } }}
+          onPageSizeChange={(pageSize) =>
+            onChangePagination(pageSize, pagination.page)
+          }
+          onPageChange={(page) => onChangePagination(pagination.pageSize, page)}
+          style={{ minHeight: "600px" }}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+        />
+      </div>
+    );
+  };
 
-	return (
-		<div
-			style={{
-				minHeight: "700px",
-				width: "100%",
-				padding: "0 5px",
-				fontFamily: "Poppins",
-			}}
-		>
-			{renderTop()}
-			{renderContent()}
-			{status.visibleModal && renderModal()}
-		</div>
-	);
+  return (
+    <div
+      style={{
+        minHeight: "700px",
+        width: "100%",
+        padding: "0 5px",
+        fontFamily: "Poppins",
+      }}
+    >
+      {renderTop()}
+      {renderContent()}
+      {status.visibleModal && renderModal()}
+    </div>
+  );
 }
 export default SubmissionManagement;
--- src/containers/UserManagement/index.js
+++ src/containers/UserManagement/index.js
@@ -11,11 +11,11 @@ import { IconButton } from "@mui/material";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import {
-	GridToolbarColumnsButton,
-	GridToolbarContainer,
-	GridToolbarDensitySelector,
-	GridToolbarExport,
-	GridToolbarFilterButton,
+  GridToolbarColumnsButton,
+  GridToolbarContainer,
+  GridToolbarDensitySelector,
+  GridToolbarExport,
+  GridToolbarFilterButton,
 } from "@mui/x-data-grid";
 import { DataGridPro, GridActionsCellItem } from "@mui/x-data-grid-pro";
 import * as React from "react";
@@ -31,266 +31,265 @@ import ModalUserManagement from "./modal/ModalUserManagement";
 import { Column } from "./model/Column";
 
 const toastMessages = {
-	WAIT: "Please wait...",
-	SUC_USER_ADDED: "Create user successful !!",
-	SUC_USER_EDITED: "Update user successful !!",
-	SUC_USER_DEL: "Delete user successful !!",
-	ERR_SERVER_ERROR: "Something went wrong, please try again !!",
+  WAIT: "Please wait...",
+  SUC_USER_ADDED: "Create user successful !!",
+  SUC_USER_EDITED: "Update user successful !!",
+  SUC_USER_DEL: "Delete user successful !!",
+  ERR_SERVER_ERROR: "Something went wrong, please try again !!",
 };
 
 function UserManagement() {
-	const { state } = useContext(UserContext);
-	const [data, setData] = useState([]);
-	const [rowId, setRowId] = useState(null);
+  const { state } = useContext(UserContext);
+  const [data, setData] = useState([]);
+  const [rowId, setRowId] = useState(null);
 
-	const [status, setStatus] = useState({
-		visibleModal: false,
-		action: "create",
-	});
+  const [status, setStatus] = useState({
+    visibleModal: false,
+    action: "create",
+  });
 
-	const [pagination, setPagination] = useState({
-		pageSize: 10,
-		page: 0,
-	});
+  const [pagination, setPagination] = useState({
+    pageSize: 10,
+    page: 0,
+  });
 
-	const [tableToolBar, setTableToolBar] = useState(false);
+  const [tableToolBar, setTableToolBar] = useState(false);
 
-	useEffect(() => {
-		if (DEV_CONFIGS.IS_DEV) {
-			setData(dataDemo);
-			setRowId(null);
-			return;
-		}
+  useEffect(() => {
+    if (DEV_CONFIGS.IS_DEV) {
+      setData(dataDemo);
+      setRowId(null);
+      return;
+    }
 
-		loadData();
-	}, [pagination]);
+    loadData();
+  }, [pagination]);
 
+  const handleOnClickToolBar = () => setTableToolBar((pre) => !pre);
 
-	const handleOnClickToolBar = () => setTableToolBar((pre) => !pre);
+  const columns = [
+    ...Column,
+    {
+      field: "actions",
+      headerName: "Action",
+      width: 75,
+      type: "actions",
+      disableColumnMenu: true,
+      sortable: false,
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<InfoOutlinedIcon />}
+          label="Detail"
+          onClick={() => onOpenModal(params.id, "detail")}
+          showInMenu
+        />,
 
-	const columns = [
-		...Column,
-		{
-			field: "actions",
-			headerName: "Action",
-			width: 75,
-			type: "actions",
-			disableColumnMenu: true,
-			sortable: false,
-			getActions: (params) => [
-				<GridActionsCellItem
-					icon={<InfoOutlinedIcon />}
-					label="Detail"
-					onClick={() => onOpenModal(params.id, "detail")}
-					showInMenu
-				/>,
+        <GridActionsCellItem
+          icon={<EditIcon />}
+          label="Update"
+          disabled={state?.dataUser?.id === params.id ? true : false}
+          onClick={() => onOpenModal(params.id, "update")}
+          showInMenu
+        />,
 
-				<GridActionsCellItem
-					icon={<EditIcon />}
-					label="Update"
-					disabled={state?.dataUser?.id === params.id ? true : false}
-					onClick={() => onOpenModal(params.id, "update")}
-					showInMenu
-				/>,
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          disabled={state?.dataUser?.id === params.id ? true : false}
+          label="Delete"
+          onClick={() => onDelete(params.id)}
+          showInMenu
+        />,
+      ],
+    },
+  ];
 
-				<GridActionsCellItem
-					icon={<DeleteIcon />}
-					disabled={state?.dataUser?.id === params.id ? true : false}
-					label="Delete"
-					onClick={() => onDelete(params.id)}
-					showInMenu
-				/>,
-			],
-		},
-	];
+  const loadData = async () => {
+    await AuthRequest.get(API_PATHS.ADMIN.MANAGE_USER, {
+      params: {
+        page: pagination.page + 1,
+        page_size: pagination.pageSize,
+      },
+    })
+      .then((res) => {
+        setData(res?.data?.result?.rows);
+        setRowId(null);
+      })
+      .catch(() =>
+        toast.error(toastMessages.ERR_SERVER_ERROR, {
+          style: { width: "auto" },
+        })
+      );
+  };
 
-	const loadData = async () => {
-		await AuthRequest.get(API_PATHS.ADMIN.MANAGE_USER, {
-			params: {
-				page: pagination.page + 1,
-				page_size: pagination.pageSize,
-			},
-		})
-			.then((res) => {
-				setData(res?.data?.result?.rows);
-				setRowId(null);
-			})
-			.catch(() =>
-				toast.error(toastMessages.ERR_SERVER_ERROR, {
-					style: { width: "auto" },
-				}),
-			);
-	};
+  const onOpenModal = (id, action) => {
+    if (id) {
+      setRowId(id);
+    }
+    setStatus({ ...status, visibleModal: true, action });
+  };
 
-	const onOpenModal = (id, action) => {
-		if (id) {
-			setRowId(id);
-		}
-		setStatus({ ...status, visibleModal: true, action });
-	};
+  const onDelete = async (id) => {
+    toast
+      .promise(
+        AuthRequest.delete(`${API_PATHS.ADMIN.MANAGE_USER}/${id}`)
+          .then(() => sleep(700))
+          .catch(),
+        {
+          pending: toastMessages.WAIT,
+          success: toastMessages.SUC_USER_DEL,
+          error: toastMessages.ERR_SERVER_ERROR,
+        }
+      )
+      .then(() => {
+        setStatus({ ...status, visibleModal: false });
+        loadData();
+      });
+  };
 
-	const onDelete = async (id) => {
-		toast
-			.promise(
-				AuthRequest.delete(`${API_PATHS.ADMIN.MANAGE_USER}/${id}`)
-					.then(() => sleep(700))
-					.catch(),
-				{
-					pending: toastMessages.WAIT,
-					success: toastMessages.SUC_USER_DEL,
-					error: toastMessages.ERR_SERVER_ERROR,
-				},
-			)
-			.then(() => {
-				setStatus({ ...status, visibleModal: false });
-				loadData();
-			});
-	};
+  const onUpdate = async (value) => {
+    toast
+      .promise(
+        AuthRequest.put(`${API_PATHS.ADMIN.MANAGE_USER}/${value?.id}`, value)
+          .then(() => sleep(700))
+          .catch(),
+        {
+          pending: toastMessages.WAIT,
+          success: toastMessages.SUC_USER_EDITED,
+          error: toastMessages.ERR_SERVER_ERROR,
+        }
+      )
+      .then(() => {
+        setStatus({ ...status, visibleModal: false });
+        loadData();
+      });
+  };
 
-	const onUpdate = async (value) => {
-		toast
-			.promise(
-				AuthRequest.put(`${API_PATHS.ADMIN.MANAGE_USER}/${value?.id}`, value)
-					.then(() => sleep(700))
-					.catch(),
-				{
-					pending: toastMessages.WAIT,
-					success: toastMessages.SUC_USER_EDITED,
-					error: toastMessages.ERR_SERVER_ERROR,
-				},
-			)
-			.then(() => {
-				setStatus({ ...status, visibleModal: false });
-				loadData();
-			});
-	};
+  const onCreate = async (value) => {
+    toast
+      .promise(
+        AuthRequest.post(API_PATHS.ADMIN.MANAGE_USER, value)
+          .then(() => sleep(700))
+          .catch(),
+        {
+          pending: toastMessages.WAIT,
+          success: toastMessages.SUC_USER_ADDED,
+          error: toastMessages.ERR_SERVER_ERROR,
+        }
+      )
+      .then(() => {
+        setStatus({ ...status, visibleModal: false });
+        loadData();
+      });
+  };
 
-	const onCreate = async (value) => {
-		toast
-			.promise(
-				AuthRequest.post(API_PATHS.ADMIN.MANAGE_USER, value)
-					.then(() => sleep(700))
-					.catch(),
-				{
-					pending: toastMessages.WAIT,
-					success: toastMessages.SUC_USER_ADDED,
-					error: toastMessages.ERR_SERVER_ERROR,
-				},
-			)
-			.then(() => {
-				setStatus({ ...status, visibleModal: false });
-				loadData();
-			});
-	};
+  const onCloseModal = () => {
+    rowId && setRowId(null);
 
-	const onCloseModal = () => {
-		rowId && setRowId(null);
+    setStatus({
+      ...status,
+      visibleModal: false,
+    });
+  };
 
-		setStatus({
-			...status,
-			visibleModal: false,
-		});
-	};
+  const CustomToolbar = () => {
+    return (
+      <GridToolbarContainer
+        sx={{ fontWeight: 700, display: "flex", justifyContent: "center" }}
+      >
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+        <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
+      </GridToolbarContainer>
+    );
+  };
 
-	const CustomToolbar = () => {
-		return (
-			<GridToolbarContainer
-				sx={{ fontWeight: 700, display: "flex", justifyContent: "center" }}
-			>
-				<GridToolbarColumnsButton />
-				<GridToolbarFilterButton />
-				<GridToolbarDensitySelector />
-				<GridToolbarExport printOptions={{ disableToolbarButton: true }} />
-			</GridToolbarContainer>
-		);
-	};
+  const renderModal = () => {
+    return (
+      <ModalUserManagement
+        visible={status.visibleModal}
+        action={status.action}
+        onClose={onCloseModal}
+        rowId={rowId}
+        onCreate={onCreate}
+        onUpdate={onUpdate}
+      />
+    );
+  };
 
-	const renderModal = () => {
-		return (
-			<ModalUserManagement
-				visible={status.visibleModal}
-				action={status.action}
-				onClose={onCloseModal}
-				rowId={rowId}
-				onCreate={onCreate}
-				onUpdate={onUpdate}
-			/>
-		);
-	};
+  const onChangePagination = (pageSize, page) => {
+    setPagination({ page, pageSize });
+  };
 
-	const onChangePagination = (pageSize, page) => {
-		setPagination({ page, pageSize });
-	};
+  const renderTop = () => {
+    return (
+      <div className="managementuser_title">
+        <div className="managementuser_heading">
+          <h2>Management User</h2>
+          <Tooltip title="Table Tool Bar">
+            <IconButton onClick={handleOnClickToolBar}>
+              <MoreVertIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
 
-	const renderTop = () => {
-		return (
-			<div className="managementuser_title">
-				<div className="managementuser_heading">
-					<h2>Management User</h2>
-					<Tooltip title="Table Tool Bar">
-						<IconButton onClick={handleOnClickToolBar}>
-							<MoreVertIcon />
-						</IconButton>
-					</Tooltip>
-				</div>
+        <Button
+          variant="contained"
+          endIcon={<AddCircleOutlineIcon />}
+          onClick={() => onOpenModal(null, "create")}
+        >
+          Add user
+        </Button>
+      </div>
+    );
+  };
 
-				<Button
-					variant="contained"
-					endIcon={<AddCircleOutlineIcon />}
-					onClick={() => onOpenModal(null, "create")}
-				>
-					Add user
-				</Button>
-			</div>
-		);
-	};
+  const renderContent = () => {
+    return (
+      <div className="managementuser_table">
+        <DataGridPro
+          components={{
+            NoRowsOverlay: CustomNoRowsOverlay,
+            ColumnSortedDescendingIcon: () => (
+              <ExpandMoreIcon className="icon" />
+            ),
+            ColumnSortedAscendingIcon: () => (
+              <ExpandLessIcon className="icon" />
+            ),
+            Toolbar: tableToolBar && CustomToolbar,
+          }}
+          rows={data}
+          columns={columns}
+          columnVisibilityModel={{}}
+          pagination={true}
+          cell--textCenter
+          pageSize={pagination.pageSize}
+          page={pagination.page}
+          initialState={{ pinnedColumns: { right: ["actions"] } }}
+          onPageSizeChange={(pageSize) =>
+            onChangePagination(pageSize, pagination.page)
+          }
+          onPageChange={(page) => onChangePagination(pagination.pageSize, page)}
+          style={{ minHeight: "600px" }}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+        />
+      </div>
+    );
+  };
 
-	const renderContent = () => {
-		return (
-			<div className="managementuser_table">
-				<DataGridPro
-					components={{
-						NoRowsOverlay: CustomNoRowsOverlay,
-						ColumnSortedDescendingIcon: () => (
-							<ExpandMoreIcon className="icon" />
-						),
-						ColumnSortedAscendingIcon: () => (
-							<ExpandLessIcon className="icon" />
-						),
-						Toolbar: tableToolBar && CustomToolbar,
-					}}
-					rows={data}
-					columns={columns}
-					columnVisibilityModel={{}}
-					pagination={true}
-					cell--textCenter
-					pageSize={pagination.pageSize}
-					page={pagination.page}
-					initialState={{ pinnedColumns: { right: ["actions"] } }}
-					onPageSizeChange={(pageSize) =>
-						onChangePagination(pageSize, pagination.page)
-					}
-					onPageChange={(page) => onChangePagination(pagination.pageSize, page)}
-					style={{ minHeight: "600px" }}
-					rowsPerPageOptions={[10, 25, 50, 100]}
-				/>
-			</div>
-		);
-	};
-
-	return (
-		<div
-			style={{
-				minHeight: "700px",
-				width: "100%",
-				padding: "0 5px",
-				fontFamily: "Poppins",
-			}}
-		>
-			{renderTop()}
-			{renderContent()}
-			{status.visibleModal && renderModal()}
-		</div>
-	);
+  return (
+    <div
+      style={{
+        minHeight: "700px",
+        width: "100%",
+        padding: "0 5px",
+        fontFamily: "Poppins",
+      }}
+    >
+      {renderTop()}
+      {renderContent()}
+      {status.visibleModal && renderModal()}
+    </div>
+  );
 }
 export default UserManagement;
