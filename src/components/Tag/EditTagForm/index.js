68e64107f0709cecbd96f8f7dc9ffe8754332c89
nghia <naovu@protonmail.com>
2022-04-03 21:37:13 +0700

    MODIFY: detail, update modal height bug, fix textare resize

--- src/components/Tag/EditTagForm/index.js
+++ src/components/Tag/EditTagForm/index.js
@@ -12,114 +12,114 @@ import { toast } from "react-toastify";
 import * as yup from "yup";
 
 const CssTextField = styled(TextField)({
-	".MuiFormHelperText-root": {
-		fontSize: "14px",
-		fontFamily: "Poppins",
-	},
+  ".MuiFormHelperText-root": {
+    fontSize: "14px",
+    fontFamily: "Poppins",
+  },
 
-	"& .MuiInputBase-root": {
-		color: "#000",
-		fontSize: "16px",
-		fontFamily: "Poppins",
-	},
-	"& label.Mui-focused": {
-		color: "#000",
-	},
-	"& .MuiInput-underline:after": {
-		borderBottomColor: "#000",
-	},
-	"& .MuiOutlinedInput-root": {
-		"& fieldset": {
-			borderRadius: "5px",
-		},
-		"&:hover fieldset": {
-			border: "1px solid #000000",
-		},
-		"&.Mui-focused fieldset": {
-			border: "1px solid #000000",
-		},
-	},
+  "& .MuiInputBase-root": {
+    color: "#000",
+    fontSize: "16px",
+    fontFamily: "Poppins",
+  },
+  "& label.Mui-focused": {
+    color: "#000",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#000",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderRadius: "5px",
+    },
+    "&:hover fieldset": {
+      border: "1px solid #000000",
+    },
+    "&.Mui-focused fieldset": {
+      border: "1px solid #000000",
+    },
+  },
 });
 
 const ColorButton = styled(Button)(() => ({
-	fontFamily: "Poppins",
-	fontSize: "13px",
-	fontWeight: "bold",
-	textTransform: "none",
-	minWidth: 200,
-	display: "inline-block",
+  fontFamily: "Poppins",
+  fontSize: "13px",
+  fontWeight: "bold",
+  textTransform: "none",
+  minWidth: 200,
+  display: "inline-block",
 
-	margin: "10px",
-	padding: "10px",
+  margin: "10px",
+  padding: "10px",
 
-	"&:disabled ": { cursor: "not-allowed", pointerEvents: "all !important" },
+  "&:disabled ": { cursor: "not-allowed", pointerEvents: "all !important" },
 }));
 
 const toastMessages = {
-	ERR_SERVER_ERROR: "Something went wrong, please try again !!",
+  ERR_SERVER_ERROR: "Something went wrong, please try again !!",
 };
 
 const validationSchema = yup.object({
-	name: yup.string().required("Full Name is required"),
+  name: yup.string().required("Full Name is required"),
 });
 
 function EditTagForm(props) {
-	const { onClose, onUpdate, initialValue } = props;
+  const { onClose, onUpdate, initialValue } = props;
 
-	const formik = useFormik({
-		initialValues: initialValue,
-		validationSchema: validationSchema,
-		onSubmit: (values) => {
-			onUpdate(values);
-		},
-	});
+  const formik = useFormik({
+    initialValues: initialValue,
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      onUpdate(values);
+    },
+  });
 
-	useEffect(() => {
-		if (formik?.values?.length < 1) {
-			toast.error(toastMessages.ERR_SERVER_ERROR, { style: { width: "auto" } });
-		}
-	}, []);
+  useEffect(() => {
+    if (formik?.values?.length < 1) {
+      toast.error(toastMessages.ERR_SERVER_ERROR, { style: { width: "auto" } });
+    }
+  }, []);
 
-	return (
-		<div className="edittagform">
-			<div className="edittagform_title">
-				<h2>Update Tag</h2>
-				<IconButton>
-					<CloseIcon onClick={() => onClose()} />
-				</IconButton>
-			</div>
-			<br />
+  return (
+    <div className="edittagform">
+      <div className="edittagform_title">
+        <h2>Update Tag</h2>
+        <IconButton>
+          <CloseIcon onClick={() => onClose()} />
+        </IconButton>
+      </div>
+      <br />
 
-			<form className="form_grid" onSubmit={formik.handleSubmit}>
-				<div className="form_group">
-					<div className="form_content">
-						<InputLabel required htmlFor="full_name">
-							Tag Name
-						</InputLabel>
-						<CssTextField
-							fullWidth
-							margin="normal"
-							id="name"
-							name="name"
-							value={formik.values.name}
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-							error={formik.touched.full_name && Boolean(formik.errors.name)}
-							helperText={formik.touched.name && formik.errors.name}
-						/>
-					</div>
-				</div>
-				<div className="edittagform_footer">
-					<ColorButton variant="outlined" onClick={() => onClose()}>
-						Cancel
-					</ColorButton>
-					<ColorButton variant="contained" type="submit">
-						Update
-					</ColorButton>
-				</div>
-			</form>
-		</div>
-	);
+      <form className="edittagform_grid" onSubmit={formik.handleSubmit}>
+        <div className="edittagform_group">
+          <div className="edittagform_content">
+            <InputLabel required htmlFor="full_name">
+              Tag Name
+            </InputLabel>
+            <CssTextField
+              fullWidth
+              margin="normal"
+              id="name"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.full_name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
+          </div>
+        </div>
+        <div className="edittagform_footer">
+          <ColorButton variant="outlined" onClick={() => onClose()}>
+            Cancel
+          </ColorButton>
+          <ColorButton variant="contained" type="submit">
+            Update
+          </ColorButton>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default React.memo(EditTagForm);
