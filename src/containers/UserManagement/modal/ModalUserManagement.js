7e525681774d640148c64de2a7f9d49ce1ad8acf
nghia <naovu@protonmail.com>
2022-04-03 22:00:21 +0700

    MODIFY: gender user type

--- src/containers/UserManagement/modal/ModalUserManagement.js
+++ src/containers/UserManagement/modal/ModalUserManagement.js
@@ -13,89 +13,89 @@ import EditUserForm from "../../../components/User/EditUserForm";
 import { dataDemo } from "../FakeData";
 
 const style = {
-	position: "relative",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: "1000px",
-	bgcolor: "background.paper",
-	boxShadow: 24,
-	p: 4,
-	borderRadius: "5px",
-	overflow: "auto",
-	maxHeight: "100%",
+  position: "relative",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "1000px",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "5px",
+  overflow: "auto",
+  maxHeight: "100%",
 
-	" @media (max-width: 1000px)": {
-		width: "100%",
-	},
+  " @media (max-width: 1000px)": {
+    width: "100%",
+  },
 };
 
 const toastMessages = {
-	ERR_SERVER_ERROR: "Something went wrong, please try again !!",
-	ERR_USER_NOT_FOUND: "User not found !!",
+  ERR_SERVER_ERROR: "Something went wrong, please try again !!",
+  ERR_USER_NOT_FOUND: "User not found !!",
 };
 
 const ModalUserManagement = (props) => {
-	const { visible, onClose, onCreate, onUpdate, action, rowId } = props;
-	const [initialValue, setInitialValue] = useState([]);
+  const { visible, onClose, onCreate, onUpdate, action, rowId } = props;
+  const [initialValue, setInitialValue] = useState([]);
 
-	useEffect(() => {
-		if (DEV_CONFIGS.IS_DEV) {
-			let user = dataDemo.find((_) => _.id === rowId);
-			if (!user) {
-				toast.error(toastMessages.ERR_USER_NOT_FOUND);
-				return;
-			}
-			setInitialValue(user);
-			return;
-		}
+  useEffect(() => {
+    if (DEV_CONFIGS.IS_OFFLINE_DEV) {
+      let user = dataDemo.find((_) => _.id === rowId);
+      if (!user) {
+        toast.error(toastMessages.ERR_USER_NOT_FOUND);
+        return;
+      }
+      setInitialValue(user);
+      return;
+    }
 
-		if (action !== "create") {
-			loadData();
-		}
-	}, [action]);
+    if (action !== "create") {
+      loadData();
+    }
+  }, [action]);
 
-	const loadData = async () => {
-		await AuthRequest.get(`${API_PATHS.ADMIN.MANAGE_USER}/${rowId}`)
-			.then((res) => setInitialValue(res?.data?.result))
-			.catch(() => toast.error(toastMessages.ERR_SERVER_ERROR));
-	};
+  const loadData = async () => {
+    await AuthRequest.get(`${API_PATHS.ADMIN.MANAGE_USER}/${rowId}`)
+      .then((res) => setInitialValue(res?.data?.result))
+      .catch(() => toast.error(toastMessages.ERR_SERVER_ERROR));
+  };
 
-	const renderForm = () => {
-		switch (action) {
-			case "create":
-				return <CreateUserForm onClose={() => onClose()} onCreate={onCreate} />;
-			case "update":
-				return (
-					<EditUserForm
-						onClose={() => onClose()}
-						onUpdate={onUpdate}
-						initialValue={initialValue}
-					/>
-				);
-			case "detail":
-				return (
-					<DetailUserForm
-						onClose={() => onClose()}
-						initialValue={initialValue}
-					/>
-				);
-			default:
-				return;
-		}
-	};
+  const renderForm = () => {
+    switch (action) {
+      case "create":
+        return <CreateUserForm onClose={() => onClose()} onCreate={onCreate} />;
+      case "update":
+        return (
+          <EditUserForm
+            onClose={() => onClose()}
+            onUpdate={onUpdate}
+            initialValue={initialValue}
+          />
+        );
+      case "detail":
+        return (
+          <DetailUserForm
+            onClose={() => onClose()}
+            initialValue={initialValue}
+          />
+        );
+      default:
+        return;
+    }
+  };
 
-	if (action !== "create" && _.isEmpty(initialValue)) return null;
+  if (action !== "create" && _.isEmpty(initialValue)) return null;
 
-	return (
-		<Modal
-			open={visible}
-			onClose={() => onClose()}
-			aria-labelledby="modal-modal-title"
-			aria-describedby="modal-modal-description"
-		>
-			<Box sx={style}>{renderForm()}</Box>
-		</Modal>
-	);
+  return (
+    <Modal
+      open={visible}
+      onClose={() => onClose()}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>{renderForm()}</Box>
+    </Modal>
+  );
 };
 export default React.memo(ModalUserManagement);
